d23f1de7dc755607cce35731db7e64f672341e69
agawrysiuk <arkad.gawry@gmail.com>
2025-02-14 16:07:49 +0100

    Implemented card flipping with real data and the simple weighted randomizer that will choose the card to show

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import {DayData} from '../data/dto';
+import {Component} from '@angular/core';
+import {DayData, FlipCard} from '../data/dto';
 import {Router} from '@angular/router';
+
+export interface WeightedFlipCard {
+  weight: number;
+  card: FlipCard;
+}
 
 @Component({
   selector: 'app-learn',
@@ -10,24 +15,52 @@ import {Router} from '@angular/router';
 })
 export class LearnComponent {
   state = 1;
-  text1 = 'Text 1';
-  text2 = 'Text 2';
-
   data!: DayData;
+  weightedCards: WeightedFlipCard[] = [];
+  currentCard!: FlipCard;
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     if (navigation?.extras?.state) {
       this.data = navigation.extras.state['data'];
+      this.weightedCards = this.data.cards
+        .flatMap(card => [
+          card,
+          { front: card.back, back: card.front }
+        ])
+        .map(card => ({weight: 3, card}));
+      console.log(this.weightedCards)
+      this.pickCard();
     }
   }
 
-  toggleState() {
-    this.state = this.state === 1 ? 2 : 1;
+  showNextCard(newWeight: number) {
+    console.log('showNextCard', newWeight);
+    this.adjustWeight(this.currentCard, newWeight);
+    this.pickCard();
+    this.toggleState();
   }
 
-  logReaction(reaction: string) {
-    console.log('User reaction:', reaction);
-    this.state = 1;
+  adjustWeight(card: FlipCard, newWeight: number) {
+    const item = this.weightedCards.find(wc => wc.card === card)!;
+    item.weight = newWeight;
+  }
+
+  pickCard() {
+    const expandedList: FlipCard[] = [];
+
+    for (const wc of this.weightedCards) {
+      for (let i = 0; i < wc.weight; i++) {
+        expandedList.push(wc.card);
+      }
+    }
+
+    const randomIndex = Math.floor(Math.random() * expandedList.length);
+    this.currentCard = expandedList[randomIndex];
+  }
+
+
+  toggleState() {
+    this.state = this.state === 1 ? 2 : 1;
   }
 }
